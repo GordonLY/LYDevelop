6e097203e8b89258bd6ea2da1c18dbe168aba632
Gordon <liyang_xx@icloud.com>
2018-02-28 10:30:41 +0800

    ...

--- Common/LYNavBgView.swift
+++ Common/LYNavBgView.swift
@@ -10,85 +10,110 @@ import UIKit
 
 class LYNavBgView: UIView {
     
-    var titleLabel: UILabel!
-    var leftClickCallBack: (() -> Void)?
-    var rightClickCallBack: (() -> Void)?
-    
-    enum BgViewLeftStyle {
+    enum bgLeftStyle {
         case back
         case close
     }
-    enum BgViewRightStyle {
+    enum bgRightStyle {
         case share
     }
+    enum bgTitleStyle {
+        case defaultStyle
+        case custom(titleColor: UIColor, bgColor: UIColor)
+    }
     
-    private var leftView: UIButton!
-    private var rightView: UIButton!
+    private var leftAction: (() -> Void)
+    private var rightAction: (() -> Void)?
     
-    override init(frame: CGRect) {
+    private(set) var title: String
+    private(set) var leftView: UIButton!
+    private(set) var titleLabel: UILabel!
+    private(set) var rightView: UIButton!
+    private(set) var line: UIView!
+    
+    init(title: String, leftAction: @escaping (() -> Void)) {
+        self.title = title
+        self.leftAction = leftAction
+        let frame = CGRect(x: 0, y: 0, width: kScreenWid(), height: kNavBottom())
         super.init(frame: frame)
+        p_initSubviews()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - ********* public method for style setting
+extension LYNavBgView {
+    func setLeftStyle(_ style: bgLeftStyle) {
+        switch style {
+        case .back:
+            leftView.setImage(UIImage.init(named: "km_back"), for: .normal)
+            leftView.setImage(UIImage.init(named: "km_back")?.ly.tintColor(kTitleColorHalf()), for: .highlighted)
+        case .close:
+            leftView.setImage(UIImage.init(named: "km_back"), for: .normal)
+            leftView.setImage(UIImage.init(named: "km_back")?.ly.tintColor(kTitleColorHalf()), for: .highlighted)
+        }
+    }
+    func setRightStyle(_ style: bgRightStyle, rightAction: (() -> Void)? = nil) {
+        self.rightAction = rightAction
+        switch style {
+        case .share:
+            rightView.setImage(UIImage.init(named: "share_icon"), for: .normal)
+            rightView.setImage(UIImage.init(named: "share_icon")?.ly.tintColor( kTitleColorHalf()), for: .highlighted)
+        }
+        rightView.isHidden = false
+    }
+    func setTitleStyle(_ style: bgTitleStyle) {
+        switch style {
+        case .defaultStyle:
+            titleLabel.textColor = kTitleColor()
+            self.backgroundColor = UIColor.white
+        case .custom(let titleColor,let bgColor):
+            titleLabel.textColor = titleColor
+            self.backgroundColor = bgColor
+        }
+    }
+}
+
+// MARK: - ********* response
+extension LYNavBgView {
+    
+    @objc fileprivate func p_actionleftView() {
+        leftAction()
+    }
+    @objc fileprivate func p_actionRightView() {
+        rightAction?()
+    }
+}
+
+extension LYNavBgView {
+    
+    fileprivate func p_initSubviews() {
         
-        self.frame = CGRect.init(x: 0, y: 0, width: kScreenWid(), height: 64)
         self.backgroundColor = UIColor.white
         
-        
-        let line = UIView.init(frame: CGRect.init(x: 0, y: 63.5, width: kScreenWid(), height: 0.5))
+        line = UIView.init(frame: CGRect.init(x: 0, y: kNavBottom() - 0.5, width: kScreenWid(), height: 0.5))
         line.backgroundColor = kSeparateLineColor()
         self.addSubview(line)
         
-        titleLabel = UILabel.init(frame: CGRect.init(x: 48, y: 20, width: kScreenWid() - 96, height: 44))
+        titleLabel = UILabel.init(frame: CGRect.init(x: 48, y: kNavTop(), width: kScreenWid() - 96, height: 44))
         titleLabel.textColor = kTitleColor()
         titleLabel.textAlignment = .center
         titleLabel.font = kStandardRegualrFont(size: 17)
         self.addSubview(titleLabel)
         
-        leftView = UIButton.init(frame: CGRect.init(x: 0, y: 20, width: 48, height: 44))
+        leftView = UIButton.init(frame: CGRect.init(x: 0, y: kNavTop(), width: 48, height: 44))
         leftView.setImage(UIImage.init(named: "km_back"), for: .normal)
-        leftView.setImage(UIImage.init(named: "km_back")?.ly_image(tintColor: kTitleColorHalf()), for: .highlighted)
-        leftView.isHidden = true
-        leftView.addTarget(self, action: #selector(p_ActionleftView), for: .touchUpInside)
+        leftView.setImage(UIImage.init(named: "km_back")?.ly.tintColor(kTitleColorHalf()), for: .highlighted)
+        leftView.addTarget(self, action: #selector(p_actionleftView), for: .touchUpInside)
         self.addSubview(leftView)
         
-        rightView = UIButton.init(frame: CGRect.init(x: kScreenWid() - 48, y: 20, width: 48, height: 44))
+        rightView = UIButton.init(frame: CGRect.init(x: kScreenWid() - 48, y: kNavTop(), width: 48, height: 44))
         rightView.setImage(UIImage.init(named: "share_icon"), for: .normal)
-        rightView.setImage(UIImage.init(named: "share_icon")?.ly_image(tintColor: kTitleColorHalf()), for: .highlighted)
+        rightView.setImage(UIImage.init(named: "share_icon")?.ly.tintColor( kTitleColorHalf()), for: .highlighted)
         rightView.isHidden = true
-        rightView.addTarget(self, action: #selector(p_ActionRightView), for: .touchUpInside)
+        rightView.addTarget(self, action: #selector(p_actionRightView), for: .touchUpInside)
         self.addSubview(rightView)
-    }
-    
-    func p_ActionleftView() {
-        if (leftClickCallBack != nil) {
-            leftClickCallBack!()
-        }
-    }
-    func p_ActionRightView() {
-        if (rightClickCallBack != nil) {
-            rightClickCallBack!()
-        }
-    }
-    
-    
-    func setLeftViewStyle(_ style: BgViewLeftStyle, callBack:@escaping () -> Void) {
-        leftClickCallBack = callBack
-        switch style {
-        case .back:
-            leftView.setImage(UIImage.init(named: "km_back"), for: .normal)
-            leftView.setImage(UIImage.init(named: "km_back")?.ly_image(tintColor: kSubTitleColorHalf()), for: .highlighted)
-        case .close:
-            leftView.setImage(UIImage.init(named: "home_close"), for: .normal)
-            leftView.setImage(UIImage.init(named: "home_close")?.ly_image(tintColor: kSubTitleColorHalf()), for: .highlighted)
-        }
-        leftView.isHidden = false
-    }
-    
-    func setRightViewStyle(_ style: BgViewRightStyle, callBack:@escaping () -> Void) {
-        rightClickCallBack = callBack
-        rightView.isHidden = false
-    }
-    
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 }
